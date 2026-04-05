9ca2d762abfb9871a7f05bb386c1cedc2b88204d
Undeavor <92637959+Undeavor@users.noreply.github.com>
2026-04-05 21:20:03 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -114,7 +114,7 @@ if st.button("Résultats backtest"):
 
     fig, ax = plt.subplots(figsize=(10,6))
     ax.plot(portefeuille_opt, label=f'Portefeuille optimisé sans frais, S={sharpe_opt:.2f}')
-    ax.plot(portefeuille_opt_frais, label=f'Portefeuille optimisé avec frais, S={sharpe_opt_frais:.2f}')
+    ax.plot(portefeuille_opt_puis_frais, label=f'Portefeuille optimisé avec frais, S={sharpe_opt_frais:.2f}')
     ax.plot(portefeuille_opt_avec_frais, label=f'Portefeuille optimisant les frais, S={sharpe_opt_avec_frais:.2f}')
     ax.plot(portefeuille_ref, label=f'Portefeuille 1/n, S={sharpe_ref:.2f}', linestyle='--')
     ax.set_title("Backtest : Portefeuille stratégie All-in")
