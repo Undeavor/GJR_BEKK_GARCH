8452f996eb018cd54eea3148489299879ee5db5d
D.D <92637959+Undeavor@users.noreply.github.com>
2026-04-21 10:58:50 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -390,6 +390,7 @@ if backtest_button:
         st.error(TEXT[lang]["no_file"])
     else:
         st.header(TEXT[lang]["results"])
+        st.header(f"Dates Backtest: {start_date} -> {end_date}")
         fig, ax = plt.subplots(figsize=(10, 6))
 
         if strategie == "allin":
